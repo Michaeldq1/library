63347618c1c3de8660538d1400b954d097d0fe51
Michaeldq1 <michaeldquinton@gmail.com>
2023-02-10 07:35:51 -0600

    functionality for add-book icon and modal-content

--- index.js
+++ index.js
@@ -1,61 +1,89 @@
 "use strict";
 
-const bookList = [
-  {
-    title: "Unfuck Yourself",
-    author: "Gary Bishop",
-    genre: "Self-Improvement",
-    pages: 224,
-  },
-];
+const bookList = [];
 const addBookButton = document.getElementById("add-book");
 const addBookModal = document.getElementById("add-book-modal");
+const submitBook = document.getElementById("submit-book");
 const bookListContainer = document.getElementById("main-container");
+const closeModalButton = document.getElementById("close-modal");
 
 addBookButton.addEventListener("click", (event) => {
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("book-card");
+  addBookModal.style.display = "block";
+});
 
-  const bookTitle = document.createElement("div");
-  bookTitle.classList.add("book-title");
-  bookTitle.textContent = bookList[0].title;
+submitBook.addEventListener("click", (event) => {
+  const book = {};
+  const bookTitle = document.getElementById("modal-input-title");
+  const bookAuthor = document.getElementById("modal-input-author");
+  const bookGenre = document.getElementById("modal-input-genre");
+  const bookPages = document.getElementById("modal-input-pages");
 
-  const bookAuthor = document.createElement("div");
-  bookAuthor.classList.add("book-author");
-  bookAuthor.textContent = bookList[0].author;
+  book.title = bookTitle.value;
+  book.author = bookAuthor.value;
+  book.genre = bookGenre.value;
+  book.pages = bookPages.value;
 
-  const bookGenre = document.createElement("div");
-  bookGenre.classList.add("book-genre");
-  bookGenre.textContent = bookList[0].genre;
+  addBookModal.style.display = "none";
+  bookList.push(book);
+  bookListContainer.innerHTML = "";
 
-  const bookPagesIcon = document.createElement("span");
-  bookPagesIcon.classList.add("material-symbols-outlined");
-  bookPagesIcon.classList.add("pages-icon");
-  bookPagesIcon.textContent = "auto_stories";
+  renderBookCards();
+  console.log(book);
+  console.log(bookList);
 
-  const bookPages = document.createElement("div");
-  bookPages.classList.add("pages");
-  bookPages.textContent = bookList[0].pages;
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookGenre.value = "";
+  bookPages.value = "";
+});
 
-  const bookRead = document.createElement("span");
-  bookRead.classList.add("material-symbols-outlined");
-  bookRead.classList.add("book-read");
-  bookRead.textContent = "check_circle";
+function renderBookCards() {
+  for (const book of bookList) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
 
-  const bookFavorite = document.createElement("span");
-  bookFavorite.classList.add("material-symbols-outlined");
-  bookFavorite.classList.add("book-favorite");
-  bookFavorite.textContent = "star";
+    const bookTitle = document.createElement("div");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
 
-  bookCard.appendChild(bookTitle);
-  bookCard.appendChild(bookAuthor);
-  bookCard.appendChild(bookGenre);
-  bookCard.appendChild(bookPagesIcon);
-  bookCard.appendChild(bookPages);
-  bookCard.appendChild(bookRead);
-  bookCard.appendChild(bookFavorite);
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.textContent = book.author;
 
-  bookListContainer.appendChild(bookCard);
+    const bookGenre = document.createElement("div");
+    bookGenre.classList.add("book-genre");
+    bookGenre.textContent = book.genre;
 
-  // addBookModal.style.display = "block";
+    const bookPagesIcon = document.createElement("span");
+    bookPagesIcon.classList.add("material-symbols-outlined");
+    bookPagesIcon.classList.add("pages-icon");
+    bookPagesIcon.textContent = "auto_stories";
+
+    const bookPages = document.createElement("div");
+    bookPages.classList.add("pages");
+    bookPages.textContent = book.pages;
+
+    const bookRead = document.createElement("span");
+    bookRead.classList.add("material-symbols-outlined");
+    bookRead.classList.add("book-read");
+    bookRead.textContent = "check_circle";
+
+    const bookFavorite = document.createElement("span");
+    bookFavorite.classList.add("material-symbols-outlined");
+    bookFavorite.classList.add("book-favorite");
+    bookFavorite.textContent = "star";
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookGenre);
+    bookCard.appendChild(bookPagesIcon);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    bookCard.appendChild(bookFavorite);
+    bookListContainer.appendChild(bookCard);
+  }
+}
+
+closeModalButton.addEventListener("click", (event) => {
+  addBookModal.style.display = "none";
 });
